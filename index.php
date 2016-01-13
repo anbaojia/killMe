7a1c99185c62c91d604ccfdd7d61981d0cb79458
An <604980258@qq.com>
2016-01-13 18:07:19 +0800

    index.php+二维数组按照某一个字段排序

--- index.php
+++ index.php
@@ -1,13 +1,42 @@
 <?php 
-/**
-* 
-*/
-class ClassName extends AnotherClass
-{
-	
-	function __construct(argument)
-	{
-		# code...
+	// class Index{
+	function index(&$array,$field,$desc = true){
+		$fieldArray = [];
+		foreach ($array as $k => $v) {
+			$fieldArray[$k] = $v[$field];
+		}
+		$sort = $desc === false?SORT_ASC:SORT_DESC;
+		array_multisort($fieldArray,$sort,$array);
+		return $array;
 	}
-}
+	// }
+	$array = array(
+		0=>array(
+			'name'=>'anbaojia',
+			'age'=>28,
+		),
+		1=>array(
+			'name'=>'anbaojia',
+			'age'=>29,
+		),
+		2=>array(
+			'name'=>'anbaojia',
+			'age'=>128,
+		),
+		3=>array(
+			'name'=>'anbaojia',
+			'age'=>8,
+		),
+		4=>array(
+			'name'=>'anbaojia',
+			'age'=>18,
+		),
+		5=>array(
+			'name'=>'anbaojia',
+			'age'=>38,
+		),
+	);
+	// $index = new Index;
+	$array = index($array,'age',false);
+	var_dump($array);
  ?>
